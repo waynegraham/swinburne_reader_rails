29d04004e6d30539154280bb19debdb9045f487c
Wayne Graham <wayne.graham@virginia.edu>
2015-07-09 14:15:52 -0400

    updates and stuff

--- app/models/poem.rb
+++ app/models/poem.rb
@@ -1,4 +1,12 @@
 class Poem < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  def next
+    Poem.where("id > ?", id).order('id ASC').first || Poem.first
+  end
+
+  def prev
+    Poem.where("id > ?", id).order('id DESC').first || Poem.last
+  end
 end
